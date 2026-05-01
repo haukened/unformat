15bdb612946d68ae6a3dc52fbef7737f793468a5
David Haukeness <david@hauken.us>
2026-05-01 10:34:46 -0600

    initial project

---
+++ unformat/unformatApp.swift
@@ -0,0 +1,17 @@
+//
+//  unformatApp.swift
+//  unformat
+//
+//  Created by David Haukeness on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct unformatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
